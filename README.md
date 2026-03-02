188a8624669f3986acef8c9c4ef00f545d38853e
izzyadora <souzad.isadora@gmail.com>
2026-03-02 13:09:29 -0300

    added sprites and study_session() configured in database

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Tamagochi 
+# Tamagotchi 
 
 Um projeto criado durante as férias de 2025.3, usando a linguagem Python, as bibliotecas PyGame, Time e o banco de dados SQLite3.
--- model/database.py
+++ model/database.py
@@ -1,6 +1,18 @@
 import sqlite3
 
 #cria um banco de dados
-connection = sqlite3.connect('database.db')
+def initialize_database():
+    connection = sqlite3.connect('database.db')
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
+
+    command1 = """CREATE TABLE IF NOT EXISTS study_sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject TEXT NOT NULL,
+        duration_minutes INTEGER NOT NULL,
+        start_timestamp INTEGER NOT NULL,
+        end_timestamp INTEGER NOT NULL
+        );
+    """
+    cursor.execute(command1)
+    connection.commit()
